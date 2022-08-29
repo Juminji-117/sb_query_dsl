f27f3155705057f9503576579544b4945e13225e
Ju, Minji <alswl0w0@gmail.com>
2022-08-29 15:20:19 +0900

    feat : 가장 오래된 순으로 전체 회원 조회

--- src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/ll/exam/qsl/user/repository/UserRepositoryCustom.java
@@ -2,9 +2,12 @@ package com.ll.exam.qsl.user.repository;
 
 import com.ll.exam.qsl.user.entity.SiteUser;
 
+import java.util.List;
+
 public interface UserRepositoryCustom {
     SiteUser getQslUser(Long id);
     long getQslCount();
     SiteUser getQslUserOrderByIdAscOne();
+    List<SiteUser> getQslUsersOrderByIdAsc();
 
 }
