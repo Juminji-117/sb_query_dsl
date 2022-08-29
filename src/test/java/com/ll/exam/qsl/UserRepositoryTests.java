be272a4e7d8d520ba8a7070ee64a6487153708d6
Ju, Minji <alswl0w0@gmail.com>
2022-08-29 13:35:19 +0900

    test : TestInitData 도입

--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@ActiveProfiles("test") // 테스트 모드 활성화
 @SpringBootTest
 class UserRepositoryTests {
     @Autowired
@@ -19,21 +20,21 @@ class UserRepositoryTests {
     @Test
     @DisplayName("회원 생성")
     void t1() {
-        SiteUser u1 = SiteUser.builder()
-                .username("user1")
+        SiteUser u3 = SiteUser.builder()
+                .username("user3")
                 .password("{noop}1234")
-                .email("user1@test.com")
+                .email("user3@test.com")
                 .build();
 
-        SiteUser u2 = SiteUser.builder()
-                .username("user2")
+        SiteUser u4 = SiteUser.builder()
+                .username("user4")
                 .password("{noop}1234")
-                .email("user2@test.com")
+                .email("user4@test.com")
                 .build();
 
-        // SiteUser u2 = new SiteUser(null, "user2", "{noop}1234", "user2@test.com");
+        // SiteUser u4 = new SiteUser(null, "user4", "{noop}1234", "user4@test.com");
 
-        userRepository.saveAll(Arrays.asList(u1, u2));
+        userRepository.saveAll(Arrays.asList(u3, u4));
     }
 
     @Test
